3d2b922f7d0daaf1b78d6f92c593dc95399a6ddd
savenkosy <savenko.sy@gmail.com>
2016-05-17 13:40:02 +0300

    Add java snippet

---
+++ ten.java
@@ -0,0 +1,23 @@
+class Spade {
+  public int value() {
+    return 10;
+  }
+}
+
+class Heart {
+  public int value() {
+    return 10;
+  }
+}
+
+class Diamond {
+  public int value() {
+    return 10;
+  }
+}
+
+class Club {
+  public int value() {
+    return 10;
+  }
+}

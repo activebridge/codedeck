f2fbb85fdf99511299e2d51a3af90e9761b84c3e
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-05-17 23:46:55 +0300

    add Bash cards

---
+++ six.sh
@@ -0,0 +1,15 @@
+ssh card
+'su - spades;
+export rank = six'
+
+ssh card
+'su - hearts;
+export rank = six'
+
+ssh card
+'su - diamonds;
+export rank = six'
+
+ssh card
+'su - clubs;
+export rank = six'

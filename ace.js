106a2fa813ae4a88a0fa0bb76b0d176613202ec4
galulex <alex@active-bridge.com>
2016-05-17 15:07:00 +0300

    add js

---
+++ ace.js
@@ -0,0 +1,23 @@
+var card = {
+  suite: 'Spade',
+  name: 'Ace',
+  color: 'black'
+};
+
+var card = {
+  suite: 'Heart',
+  name: 'Ace',
+  color: 'red'
+};
+
+var card = {
+  suite: 'Diamond',
+  name: 'Ace',
+  color: 'red'
+};
+
+var card = {
+  suite: 'Club',
+  name: 'Ace',
+  color: 'black'
+};

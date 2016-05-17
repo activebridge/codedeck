89fbcabc283d070031b01351386e15d5bdd9312f
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-05-17 17:30:50 +0300

    Add Python cards

---
+++ nine.py
@@ -0,0 +1,23 @@
+card = dict(
+  rank = 9,
+  suit = 'Spade',
+  color = 'black')
+print(card)
+
+card = dict(
+  rank = 9,
+  suit = 'Club',
+  color = 'black')
+print(card)
+
+card = dict(
+  rank = 9,
+  suit = 'Diamond',
+  color = 'red')
+print(card)
+
+card = dict(
+  rank = 9,
+  suit = 'Heart',
+  color = 'red')
+print(card)

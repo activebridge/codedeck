18994354c34a0acd85b47fb6041071148eb29dd2
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-05-17 21:54:58 +0300

    Add Scala cards

---
+++ three.scala
@@ -0,0 +1,27 @@
+object Card {
+  val suit: String = "Spades"
+  def main(a: Array[String]) {
+    println(suit, 3)
+  }
+}
+
+object Card {
+  val suit: String = "Hearts"
+  def main(a: Array[String]) {
+    println(suit, 3)
+  }
+}
+
+object Card {
+  val suit: String = "Diamonds"
+  def main(a: Array[String]) {
+    println(suit, 3)
+  }
+}
+
+object Card {
+  val suit: String = "Clubs"
+  def main(a: Array[String]) {
+    println(suit, 3)
+  }
+}

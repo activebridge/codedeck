e6e990b84d32ab935bc9416a1f194f3fcaebf15b
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-05-17 15:11:52 +0300

    Add PHP cards

---
+++ eight.php
@@ -0,0 +1,19 @@
+$card = array(8);
+array_push($card, 'Spade');
+array_push($card, 'black');
+print_r($card);
+
+$card = array(8);
+array_push($card, 'Club');
+array_push($card, 'black');
+print_r($card);
+
+$card = array(8);
+array_push($card, 'Diamond');
+array_push($card, 'red');
+print_r($card);
+
+$card = array(8);
+array_push($card, 'Heart');
+array_push($card, 'red');
+print_r($card);

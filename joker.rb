a56d9cc8249475be8b2d2b0f5c58c88a9a64da58
Igor Bilan <igorvbilan@gmail.com>
2016-05-18 15:07:16 +0300

    joker pre-last version

--- joker.rb
+++ joker.rb
@@ -1,25 +1,13 @@
-BasicObject.send(:define_method, 'jocker') do
-  'FUCK everething, I am RED Jocker'
-end
-BasicObject.new.jocker
-
-class Joker
-  attr_reader :name, :color
-
-  def initialize
-    name, color ='Joker', 'Red'
-  end
-end
-
-class BasicObject
-  def joker
-    'RED Joker here!'
-  end
-end
-
 class Joker < Struct.new(:color)
   def use
-    'used'
+    'RED JOKER has been used.'
   end
 end
 Joker.new('red').use
+
+class Joker < Struct.new(:color)
+  def use
+    'BLACK JOKER has been used.'
+  end
+end
+Joker.new('black').use

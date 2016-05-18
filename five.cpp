be79041a5ee57166846e0d17237563c84d29616f
alex-pro <a.partitskiy@gmail.com>
2016-05-18 13:59:20 +0300

    five

---
+++ five.cpp
@@ -0,0 +1,27 @@
+class Card {
+  public:
+    string suit() { return "Spades"; }
+    string name() { return "Five"; }
+    string color() { return "black"; }
+};
+
+class Card {
+  public:
+    string suit() { return "Hearts"; }
+    string name() { return "Five"; }
+    string color() { return "red"; }
+};
+
+class Card {
+  public:
+    string suit() { return "Diamonds"; }
+    string name() { return "Five"; }
+    string color() { return "red"; }
+};
+
+class Card {
+  public:
+    string suit() { return "Clubs"; }
+    string name() { return "Five"; }
+    string color() { return "black"; }
+};

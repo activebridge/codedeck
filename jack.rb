06567992423c9cdac5425ced9df4e836cc6ce1c4
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-05-19 14:43:52 +0300

    add Ruby cards

---
+++ jack.rb
@@ -0,0 +1,31 @@
+class Jack
+  def initialize(suit)
+    puts "#{suit} black"
+  end
+end
+
+Jack.new('Spades')
+
+class Jack
+  def initialize(suit)
+    puts "#{suit} red"
+  end
+end
+
+Jack.new('Hearts')
+
+class Jack
+  def initialize(suit)
+    puts "#{suit} red"
+  end
+end
+
+Jack.new('Diamonds')
+
+class Jack
+  def initialize(suit)
+    puts "#{suit} black"
+  end
+end
+
+Jack.new('Clubs')
---
+++ jack2.rb
@@ -0,0 +1,35 @@
+class Jack
+  attr_accessor :suit
+
+  def initialize
+    suit = 'Spades'
+    puts "#{suit} black"
+  end
+end
+
+class Jack
+  attr_accessor :suit
+
+  def initialize
+    suit = 'Hearts'
+    puts "#{suit} red"
+  end
+end
+
+class Jack
+  attr_accessor :suit
+
+  def initialize
+    suit = 'Diamonds'
+    puts "#{suit} red"
+  end
+end
+
+class Jack
+  attr_accessor :suit
+
+  def initialize
+    suit = 'Clubs'
+    puts "#{suit} black"
+  end
+end
